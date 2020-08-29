9d16c4aa5d0bf994885fa3bef0c9af6733d0aad1
Drew Thompson <drewallynthompson@gmail.com>
2020-08-29 02:45:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,8 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
+  badges = batch_badge_creator(array)
+  
+  badges.each {|array| puts array}
   puts assign_rooms(array)
 end
