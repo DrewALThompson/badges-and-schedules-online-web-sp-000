eebd86d95ad72a9a1f4bf50bdd387980da453ed5
Drew Thompson <drewallynthompson@gmail.com>
2020-08-29 01:54:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+ newbie = []
+ array.each.with_index(1) {|name, index| newbie << "Hello, #{name}! You'll be assigned to room #{index}!"}
+ newbie
+end
+
+def printer
+  
+end
